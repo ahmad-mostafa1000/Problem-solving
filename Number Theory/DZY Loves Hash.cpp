e33be483905c78d71d263bc68f8883fd4b11b308
Ahmad Mostafa <ahmad.essa94@eng-st.cu.edu.eg>
2018-08-04 11:55:37 +0200

    Initial commit,to be updates

---
+++ Number Theory/DZY Loves Hash.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int  p, temp,n, count = 0;
+//	vector<int> arr(300);
+//
+//	cin >> p;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		count++;
+//		cin >> temp;
+//		temp = (temp%p);
+//		if (arr[temp] == 1)
+//		{
+//			cout << count;
+//			system("pause");
+//			return 0;
+//		}
+//		else arr[temp] = 1;
+//	}
+//
+//	cout << -1;
+//	system("pause");
+//	return 0;
+//}
---
+++ Number Theory/Turing Tape.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	int  n, count = 0;
+//	string seq;
+//	int x = int('H');
+//	cout << x;
+//
+//
+//	cout << count;
+//	system("pause");
+//
+//	return 0;
+//
+//}
