a7e590f93877c1dde53fc95f4bbe3d4a00bf6d75
Ahmad Mostafa <ahmad.essa94@eng-st.cu.edu.eg>
2018-08-07 19:16:52 +0200

    removed comments

--- Number Theory/Turing Tape.cpp
+++ Number Theory/Turing Tape.cpp
@@ -1,19 +1,28 @@
-//#include<iostream>
-//#include<vector>
-//#include<string>
-//using namespace std;
-//
-//int main()
-//{
-//	int  n, count = 0;
-//	string seq;
-//	int x = int('H');
-//	cout << x;
-//
-//
-//	cout << count;
-//	system("pause");
-//
-//	return 0;
-//
-//}
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int  n, count = 0;
+	string seq;
+	char x = 'H';
+	int revX = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		
+		revX <<= 1;//shift shmal
+		revX |= x & 1;
+		x >>= 1;//shift  ymeen
+
+	}
+	cout << x;
+
+
+	cout << count;
+	system("pause");
+
+	return 0;
+
+}
