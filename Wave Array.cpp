9f494073932f2a28f078ef5cde78f265c6702955
Ahmad Mostafa <ahmad.essa94@eng-st.cu.edu.eg>
2018-09-10 12:02:39 +0200

    solution

--- Wave Array.cpp
+++ Wave Array.cpp
@@ -13,26 +13,29 @@ int main()
 	vector<int>input = {6,4,1,2,1,7,10};
 	vector<int>wave;
 
-	std::sort(input.begin(), input.begin() +input.size());
+	std::sort(input.begin(), input.begin() +input.size()); 
 	
 	for (int i=0;i<input.size();i++)
 	{
-		if (i % 2 == 0 && i+1<=input.size()-1)
+		if (i % 2 == 0 && i+1<=input.size()-1) //even indices
 		{
 			wave.push_back(input[i+1]);
 			cout << input[i + 1];
 		}
-		else if(i % 2 != 0)
+		else if(i % 2 != 0)//odd indices
 		{
 			wave.push_back(input[i - 1]);
 			cout << input[i -1];
 		}
 		else
 		{
-			wave.push_back(input[i]);
+			wave.push_back(input[i]);//last element
 			cout << input[i];
 		}
 	}
+
+	//Another solution: 1) sort array
+				//      2) swap each two adjacent elements
 	system("pause");
 	return 0;
 }
