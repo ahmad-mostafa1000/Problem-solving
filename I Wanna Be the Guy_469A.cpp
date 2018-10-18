b23cb93131d5a4e1362431c4a2ec81514fee618e
Ahmad Mostafa <ahmad.essa94@eng-st.cu.edu.eg>
2018-10-18 22:07:26 +0200

    solution

---
+++ I Wanna Be the Guy_469A.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	 int n, p, leveltopass[101],level,count=0;
+	cin >> n;
+	cin>>p;
+	for (int i = 0; i < 2*p; i++)
+	{
+		cin >> level;
+		if(leveltopass[level]!= 1)
+		{
+			leveltopass[level] = 1;
+			count++;
+		}
+	}
+	(count == n)?cout<<"I become the guy.": cout<<"Oh, my keyboard!";
+	
+
+	system("pause");
+	return 0;
+}
+
